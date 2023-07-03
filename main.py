099dde51700bdfc5d2f4fe6f3a38115df9b0a2ae
pdb-94 <paulbohn94@gmail.com>
2023-07-03 18:26:01 +0200

    add PV & wind turbine advanced method

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ def demonstration(grid_connection=True):
         name = 'Off grid system'
     # Add parameters to create environment
     environment = Environment(name=name,
-                              location={'latitude': 6.0442,
-                                        'longitude': -0.7983,
+                              location={'latitude': -6.045612,
+                                        'longitude': -45.31375,
                                         'altitude': 100,
-                                        'terrain': 'Agricultural terrain with many houses, bushes, plants or 8 meter '
-                                                   'high hedges at a distance of approx. 250 meters'},
+                                        'terrain': 'Villages, small towns, agricultural buildings with many or high '
+                                                   'hedges, woods and very rough and uneven terrain'},
                               time={'start': dt.datetime(year=2022,
                                                          month=1,
                                                          day=1,
@@ -35,13 +35,13 @@ def demonstration(grid_connection=True):
                                                        hour=23,
                                                        minute=59),
                                     'step': dt.timedelta(minutes=15),
-                                    'timezone': 'Etc/GMT'},
+                                    'timezone': 'Etc/GMT-4'},
                               economy={'d_rate': 0.03,
                                        'lifetime': 20,
-                                       'electricity_price': 0.375,
-                                       'diesel_price': 1.56,
-                                       'pv_feed_in_tariff': 0.07,
-                                       'wt_feed_in_tariff': 0.07,
+                                       'electricity_price': 0.152,
+                                       'diesel_price': 1.06,
+                                       'pv_feed_in_tariff': 0,
+                                       'wt_feed_in_tariff': 0,
                                        'co2_price': 0,
                                        'currency': 'US$'},
                               ecology={'co2_diesel': 0.2665,
@@ -55,17 +55,18 @@ def demonstration(grid_connection=True):
     # Add load profile from csv-file
     environment.add_load(annual_consumption=150000, ref_profile='L0')  # kWh
     # Add PV system
-    environment.add_pv(p_n=60000,
-                       pv_data={'surface_tilt': 20, 'surface_azimuth': 0, 'min_module_power': 300,
-                                'max_module_power': 400, 'inverter_power_range': 2500})
-    environment.add_pv(p_n=30000,
-                       pv_data={'surface_tilt': 20, 'surface_azimuth': 270, 'min_module_power': 300,
-                                'max_module_power': 400, 'inverter_power_range': 2500})
+    # environment.add_pv(p_n=60000,
+    #                    pv_data={'surface_tilt': 20, 'surface_azimuth': 0, 'min_module_power': 300,
+    #                             'max_module_power': 400, 'inverter_power_range': 2500})
+    environment.add_pv(pv_data={'surface_tilt': 20, 'surface_azimuth': 0, 'pv_module': 'ET_Solar_Industry_ET_M672320WW',
+                                'inverter': 'SMA_America__STP_62_US_41__480V_', 'modules_per_string': 94.0,
+                                'strings_per_inverter': 2})
+    # Add Windturbine
     # environment.add_wind_turbine(selection_parameters=[3000000, 4000000])
+    # environment.add_wind_turbine(turbine_data={'turbine_type': 'E-115/3200', 'hub_height': 92.0, 'p_n': 3200000})
     if not grid_connection:
-        environment.add_diesel_generator(p_n=30000,
-                                         fuel_consumption=12,
-                                         fuel_price=1.385)
+        environment.add_diesel_generator(p_n=35000,
+                                         fuel_consumption=12)
     environment.add_storage(p_n=10000,
                             c=30000,
                             soc=0.25)
@@ -76,9 +77,9 @@ def demonstration(grid_connection=True):
 start = dt.datetime.today()
 print('Create environment')
 # Off Grid system
-# env = demonstration(grid_connection=False)
+env = demonstration(grid_connection=False)
 # On Grid system
-env = demonstration(grid_connection=True)
+# env = demonstration(grid_connection=True)
 # Run Dispatch
 print(f'Run Dispatch {dt.datetime.today() - start}')
 operator = Operator(env=env)
